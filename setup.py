39fc7d2c4d99389f75aaec28152b79f7de1474e3
ValentinFigue <valentin.figue@orange.fr>
2019-10-30 14:36:06 +0100

    Structure of python module added

---
+++ setup.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+
+# Learn more: https://github.com/kennethreitz/setup.py
+
+from setuptools import setup, find_packages
+
+
+with open('README.rst') as f:
+    readme = f.read()
+
+with open('LICENSE') as f:
+    license = f.read()
+
+setup(
+    name='code',
+    version='0.1.0',
+    description='Sample package for Python-Guide.org',
+    long_description=readme,
+    author='Valentin Figué',
+    author_email='valentin.figue@polytechnique.edu',
+    url='https://github.com/ValentinFigue/Sklearn-PyTorch',
+    license=license,
+    packages=find_packages(exclude=('tests', 'docs'))
+)
+
