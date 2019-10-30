cac8e90bbb596020e87c8748f546d087a48b9654
ValentinFigue <valentin.figue@orange.fr>
2019-10-30 15:21:38 +0100

    Modification of setup

--- setup.py
+++ setup.py
@@ -1,11 +1,9 @@
 # -*- coding: utf-8 -*-
 
-# Learn more: https://github.com/kennethreitz/setup.py
-
 from setuptools import setup, find_packages
 
 
-with open('README.rst') as f:
+with open('README.md') as f:
     readme = f.read()
 
 with open('LICENSE') as f:
