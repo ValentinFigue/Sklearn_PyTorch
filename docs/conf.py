ed69de504ad87d7593cdb7e9c32550a9dcba4e85
Valou64 <valentin.figue@orange.fr>
2019-11-09 12:21:52 +0100

    Update of conf.py files

--- docs/conf.py
+++ docs/conf.py
@@ -40,8 +40,8 @@ source_suffix = '.rst'
 master_doc = 'index'
 
 # General information about the project.
-project = u'code'
-copyright = u'2012, Kenneth Reitz'
+project = u'Sklearn-Pytorch'
+copyright = u'2019, Valentin Figué'
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
@@ -183,8 +183,8 @@ latex_elements = {
 # Grouping the document tree into LaTeX files. List of tuples
 # (source start file, target name, title, author, documentclass [howto/manual]).
 latex_documents = [
-  ('index', 'code.tex', u'code Documentation',
-   u'Kenneth Reitz', 'manual'),
+  ('index', 'Sklearn-Pytorch.tex', u'Sklearn-Pytorch Documentation',
+   u'Valentin Figué', 'manual'),
 ]
 
 # The name of an image file (relative to this directory) to place at the top of
@@ -213,8 +213,8 @@ latex_documents = [
 # One entry per manual page. List of tuples
 # (source start file, name, description, authors, manual section).
 man_pages = [
-    ('index', 'code', u'code Documentation',
-     [u'Kenneth Reitz'], 1)
+    ('index', 'Sklearn-Pytorch', u'Sklearn-Pytorch Documentation',
+     [u'Valentin Figué'], 1)
 ]
 
 # If true, show URL addresses after external links.
@@ -227,8 +227,8 @@ man_pages = [
 # (source start file, target name, title, author,
 #  dir menu entry, description, category)
 texinfo_documents = [
-  ('index', 'code', u'code Documentation',
-   u'Kenneth Reitz', 'code', 'One line description of project.',
+  ('index', 'Sklearn-Pytorch', u'Sklearn-Pytorch Documentation',
+   u'Valentin Figué', 'Sklearn-Pytorch', 'Implementation of Sklearn model in Pytorch.',
    'Miscellaneous'),
 ]
 
